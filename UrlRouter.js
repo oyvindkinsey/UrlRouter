d380f1c201a818c086d8931f2394c9412873627e
oyvind.kinsey <oyvind@kinsey.no>
2011-06-20 16:24:43 +0200

    Refactored, commented and documented the code

--- UrlRouter.js
+++ UrlRouter.js
@@ -1,31 +1,64 @@
+
+/**
+ * UrlRouter is a simple router that can be used to provide navigation
+ * in Javascript applications.
+ *
+ * The router lets you define the patterns for url matching, and also allows you
+ * to name the variable sections so that the handler can easily access these.
+ * @param {Array} rules The rules for matching.
+ */
 function UrlRouter(rules){
+    if (!rules || rules.length === 0) {
+        throw new Error("UrlRouter: Rules missing");
+    }
     this._rules = rules;
+    this._compiled = [];
 }
 
 UrlRouter.prototype = {
+
+    /**
+     * This iterates over the literal rules, and compiles these into RegExp's,
+     * replacing the variable placeholdes with capture groups for quick matching.
+     * The varible placeholders are stored in the same order as the capture groups
+     * they represent, enabling the `match` method to easily map captured data to properties.
+     */
     compile: function(){
-        var compiled = [], rules = this._rules;
-        for (var i = 0, len = rules.length; i < len; i++) {
-            var rule = rules[i];
-            var compiledRule = {
-                keys: []
-            };
-            compiledRule.re = new RegExp(rule.replace(/:[^\/]+/g, function(key){
-                compiledRule.keys.push((key.substring(1)))
+        for (var i = 0, len = this._rules.length; i < len; i++) {
+            var keys = [];
+            // create a new RegExp with matching capture groups
+            var re = new RegExp(this._rules[i][0].replace(/:[^\/]+/g, function(key){
+                keys.push(key.substring(1));
                 return "([^\/]+)";
             }));
-            compiled.push(compiledRule);
+            this._compiled.push({
+                re: re, // the RegExp for fast matching
+                keys: keys, // the keys for mapping capture groups to property names
+                handler: this._rules[i][1] // the handler for the match
+            });
         }
-        this._compiled = compiled;
     },
+    
+    /**
+     * This iterates over the stored rules, testing each in sequence until
+     * they have all been tried or a successfull match occurs.
+     * When a match is found, the corresponding handler is called with a single argument,
+     * an object containing properties matching the variable placeholders in the matching rule.
+     * @param {String} url The string that the matching should be run against
+     * @return {Object || undefined} If successfull, returns the data object after executing the handler
+     */
     match: function(url){
-        var match;
         for (var i = 0, len = this._compiled.length; i < len; i++) {
-            match = url.match(this._compiled[i].re);
+            var match = url.match(this._compiled[i].re);
             if (match) {
-                var data = {}, keys = this._compiled[i].keys;
+                var data = {}, keys = this._compiled[i].keys, handler = this._compiled[i].handler;
+                // map capture groups to properties
                 for (var j = 0; j < keys.length; j++) {
                     data[keys[j]] = match[j + 1];
+                }
+                // call the handler, if present
+                if (handler) {
+                    handler(data);
                 }
                 return data;
             }
